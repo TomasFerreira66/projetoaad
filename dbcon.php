4e41a3dedaf2f09d6a4099faebf505b914cbed62
TomasFerreira66 <tomasff2002@gmail.com>
2024-01-10 16:40:17 +0000

    projeto AAD

---
+++ dbcon.php
@@ -0,0 +1,18 @@
+<?php
+
+define("HOSTNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DATABASE", "projetoaad");
+
+$connection = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE);
+
+if(!$connection){
+    die("Connection Failed");
+
+} else {
+
+    echo "yeess";
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<h1 id="main_title">Aplicação CRUD em PHP</h1>
+
+<div class="container">
+<h2>Apartamentos</h2>
+<table class="table table-hover table-bordered table-striped">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+        </tr>
+    </thead>
+
+    <tbody>
+    <tr>
+            <td>1</th>
+            <td>Tomas</th>
+            <td>Ferreira</th>
+            <td>21</th>
+    </tr>
+
+    <tr>
+            <td>2</th>
+            <td>Nelson</th>
+            <td>Araujo</th>
+            <td>32</th>
+    </tr>
+        
+    </tbody>
+
+</table>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+</html>
